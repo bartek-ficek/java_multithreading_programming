bca2a50fc44af012186c407896b3b12e0f917898
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-24 17:28:08 +0100

    example of Threads - priorities set, changed to lambda expressions

--- src/main/java/example/ThreadExample.java
+++ src/main/java/example/ThreadExample.java
@@ -4,13 +4,9 @@ public class ThreadExample {
 
     public static void main(String[] args) {
 
-        Thread th1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                for (int i = 0; i < 1000; i++) {
-                    System.out.println(Thread.currentThread().getName() + " " + i);
-                }
+        Thread th1 = new Thread(()-> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(Thread.currentThread().getName() + " " + i);
             }
         });
 
@@ -24,6 +20,8 @@ public class ThreadExample {
             }
         });
 
+        th1.setPriority(1);
+        th2.setPriority(10);
         th1.setName("th1");
         th2.setName("th2");
         th1.start();
