ada01e327bcdb5f336cd79624c6de6dbc034d4f6
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-24 17:56:24 +0100

    example of Threads - ExecutorService to create Threads

--- src/main/java/example/ThreadExample.java
+++ src/main/java/example/ThreadExample.java
@@ -1,31 +1,48 @@
 package example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadExample {
 
     public static void main(String[] args) {
 
-        Thread th1 = new Thread(()-> {
-            for (int i = 0; i < 1000; i++) {
-                System.out.println(Thread.currentThread().getName() + " " + i);
-            }
-        });
 
+        /*
+        ExecutorService - better method of creating Threads in bigger application.
+        We can choose number of used Threads to execute task(s).
+        We call this threads as threads 'Pool'
+         */
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(ThreadExample::incrementTo1000);
+        executorService.submit(ThreadExample::incrementTo1000);
+        executorService.submit(ThreadExample::incrementTo1000);
+        executorService.shutdown();
+
+
+        /* Standard method of creating Threads. Useful only for small tasks. Not well
+        to multithreading applications
+         */
+        Thread th1 = new Thread(ThreadExample::incrementTo1000);
         Thread th2 = new Thread(new Runnable() {
             @Override
             public void run() {
-
-                for (int i = 0; i < 1000; i++) {
-                    System.out.println(Thread.currentThread().getName() + " " + i);
-                }
+                incrementTo1000();
             }
         });
 
-        th1.setPriority(1);
-        th2.setPriority(10);
-        th1.setName("th1");
-        th2.setName("th2");
-        th1.start();
-        th2.start();
+//        th1.setPriority(1);
+//        th2.setPriority(10);
+//        th1.setName("th1");
+//        th2.setName("th2");
+//        th1.start();
+//        th2.start();
+    }
+
+    private static void incrementTo1000() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
     }
 }
 
