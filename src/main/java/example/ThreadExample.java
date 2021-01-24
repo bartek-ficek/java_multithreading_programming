8bfd7e953ce5870377929d28e7b83342c7603e51
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-24 17:16:05 +0100

    example of Threads started

--- src/main/java/example/ThreadExample.java
+++ src/main/java/example/ThreadExample.java
@@ -1,4 +1,35 @@
 package example;
 
 public class ThreadExample {
+
+    public static void main(String[] args) {
+
+        Thread th1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println(Thread.currentThread().getName() + " " + i);
+                }
+            }
+        });
+
+        Thread th2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println(Thread.currentThread().getName() + " " + i);
+                }
+            }
+        });
+
+        th1.setName("th1");
+        th2.setName("th2");
+        th1.start();
+        th2.start();
+    }
 }
+
+
+
